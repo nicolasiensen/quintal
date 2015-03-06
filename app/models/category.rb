db30207fabc51d8bdaa4528422320ff3738938b7
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-03-06 20:08:07 -0300

    Change Category#to_param method to represent the slug

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   validates :name, :slug, presence: true
   validates :slug, uniqueness: true
+
+  def to_param
+    self.slug
+  end
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe Category, type: :model do
   it { should validate_presence_of :name }
   it { should validate_presence_of :slug }
   it { should validate_uniqueness_of :slug }
+
+  describe "#to_param" do
+    it "should be the slug" do
+      category = Category.make!
+      expect(category.to_param).to be_eql(category.slug)
+    end
+  end
 end
