660dc75d2526b4f5cddb0cdf5888f887433a8c86
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-03-09 21:50:28 -0300

    Add body to post and create tables tags and posts_tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150306215923) do
+ActiveRecord::Schema.define(version: 20150310004248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,18 @@ ActiveRecord::Schema.define(version: 20150306215923) do
     t.string   "title"
     t.datetime "published_at"
     t.integer  "category_id"
+    t.text     "body"
+  end
+
+  create_table "posts_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
