2e10aad6d051d1856317a58fcd7b263fdfa197d2
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-03-29 11:19:04 -0300

    Added pagination with kaminari

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,10 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.order(published_at: :desc).limit(3)
+    @posts = Post.order(published_at: :desc)
 
     @category = Category.find_by(slug: params[:category_slug])
     @posts = @posts.where(category_id: @category.id) if @category.present?
+    @posts = @posts.page(params[:page]).per(3)
   end
 
   def show
