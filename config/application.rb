f5de26efc162538f8a247254336f199bd6388d52
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-04-03 12:30:08 -0300

    Added FontAwesome fonts to the assets pipeline

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module Quintal
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'components/**/*')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'components', 'fontawesome', 'fonts')
   end
 end
