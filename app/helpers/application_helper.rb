e2fb9f6becfeaf52925aabe09b458e9844009847
Nícolas Iensen <nicolas.iensen@gmail.com>
2015-04-03 11:02:03 -0300

    Added icons for categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,18 @@ module ApplicationHelper
   def category_link category
     content_tag(
       :a,
-      category.name,
       href: category_posts_path(category.slug),
       class: [:item, category == @category ? "active" : nil]
-    )
+    ) do
+      content_tag(:i, "", class: [:fa, category_icon(category.name)]) + " " + category.name
+    end
+  end
+
+  def category_icon category_name
+    return "fa-eye" if category_name == "Beleza"
+    return "fa-camera-retro" if category_name == "Lifestyle"
+    return "fa-paint-brush" if category_name == "Decor"
+    return "fa-cutlery" if category_name == "Gastronomia"
   end
 
   def categories
